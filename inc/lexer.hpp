87d0cccd754af3b60b7ccecf4feacd060c2e9f2c
Nikita Piatygorskiy <generalhammond16@gmail.com>
2022-06-08 21:52:26 +0300

    mc,lexer: add sym tables for token attrs
    Signed-off-by: Nikita Piatygorskiy <generalhammond16@gmail.com>

--- inc/lexer.hpp
+++ inc/lexer.hpp
@@ -11,17 +11,90 @@ enum token_tag {
     TOK_INTEGER,
     TOK_FLOAT,
 };
+extern const char *token_tag_table[];
 
 /* ids of operators */
 enum op_t {
-    LEFT_RND_BR,
-    RIGHT_RND_BR,
+    LEFT_RND_BR,        // (
+    RIGHT_RND_BR,       // )
+    LEFT_SQUARE_BR,     // [
+    RIGHT_SQUARE_BR,    // ]
+    ARROW,              // ->
+    DOT,                // .
+    INCREMENT,          // ++
+    DECREMENT,          // --
+    PLUS,               // +
+    MINUS,              // -
+    NOT,                // !
+    INV,                // ~
+    AMP,                // &
+    MUL,                // *
+    DIV,                // /
+    PERCENT,            // %
+    SHL,                // <<
+    SHR,                // >>
+    LESS,               // <
+    LESS_EQ,            // <=
+    GREATER,            // >
+    GREATER_EQ,         // >=
+    EQ,                 // ==
+    NOT_EQ,             // !=
+    CARET,              // ^
+    BTW_OR,             // |
+    OR,                 // ||
+    AND,                // &&
+    QUESTION,           // ?
+    COLON,              // :
+    ASSIGN,             // =
+    ADD_ASSIGN,         // +=
+    SUB_ASSIGN,         // -=
+    MUL_ASSIGN,         // *=
+    DIV_ASSIGN,         // /=
+    REM_ASSIGN,         // %=
+    SHL_ASSIGN,         // <<=
+    SHR_ASSIGN,         // >>=
+    AND_ASSIGN,         // &=
+    XOR_ASSIGN,         // ^=
+    OR_ASSIGN,          // |=
 };
+extern const char *ops_sym_table[];
 
 /* ids of keywords */
 enum kw_t {
-
+    AUTO, 
+    DOUBLE,
+    INT,
+	STRUC,	
+	BREAK,
+	ELSE,
+	LONG,
+	SWITC,	
+	CASE,
+	ENUM,
+	REGISTER,
+	TYPEDE,	
+	CHAR,
+	EXTERN,
+	RETURN,
+	UNIO,	
+	CONST,
+	FLOAT,
+	SHORT,
+	UNSIGNE,	
+	CONTINUE,
+	FOR,
+	SIGNED,
+	VOI,	
+	DEFAULT,
+	GOTO,
+	SIZEOF,
+	VOLATIL,	
+	DO,
+	IF,
+	STATIC,
+	WHILE,
 };
+extern const char *kws_sym_table[];
 
 /*
  * stores information about single token
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -3,6 +3,59 @@
 
 using namespace lexer;
 
+const char *token_tag_table[] = 
+{
+    "keyword", "identifier", "operator",
+    "char literal", "string literal",
+    "integer literal", "floating point literal",
+};
+
+const char *ops_sym_table[] = {
+    "(", ")", "[", "]", "->", ".",
+    "++", "--", "+", "-", "!", "~",
+    "&", "*", "/", "%", "<<", ">>",
+    "<", "<=", ">", ">=", "==", "!=",
+    "^", "|", "||", "&&", "?", ":",
+    "=", "+=", "-=", "*=", "/=", "%=",
+    "<<=", ">>=", "&=", "^=", "|=",
+};
+
+
+const char *kws_sym_table[] = {
+    "auto",
+    "double",
+    "int",
+    "struct",
+    "break",        
+    "else",        
+    "long",       
+    "switch",
+    "case",         
+    "enum",        
+    "register",   
+    "typedef",
+    "char",         
+    "extern",      
+    "return",     
+    "union",
+    "const",        
+    "float",       
+    "short",      
+    "unsigned",
+    "continue",     
+    "for",         
+    "signed",     
+    "void",
+    "default",      
+    "goto",        
+    "sizeof",     
+    "volatile",
+    "do",           
+    "if",          
+    "static",     
+    "while", 
+};
+
 bool get_tokens(std::vector<std::vector<char>> const &TU_content,
 std::vector<token> &tokens)
 {
